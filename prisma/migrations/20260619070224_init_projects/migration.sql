34e076970b944c86e4dac0446601609e944eaf94
fengyenchia <yanjia20050504@gmail.com>
2026-06-19 15:03:08 +0800

    update

---
+++ prisma/migrations/20260619070224_init_projects/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "imgSrc" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
